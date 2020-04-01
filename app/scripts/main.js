b6dd5d262c4138de2e097844afd3c668355b55d4
Raymond Weitzenberg <raymondweitzenberg@avalon.localdomain>
2020-04-01 19:17:17 -0400

    all cubes spinning
    rather than creating the elements programmatically i created 20
    <a-animation> elements and targetted them programmatically

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,14 +12,9 @@ AFRAME.registerComponent("boxes-setup", {
 		const protoBox = sceneEl.querySelector("#oneBox");
 		console.log("sceneEl: " + sceneEl);
 
-		// const boxRotat = protoBox.components.animation__rotation;
-
-		// const posLimit = 17;
-		const scaleLimit = 3;
-
-		for (let i = 0; i < 1; i++) {
-			// let posLimit = Math.random() * 27;
-			// let randDur = Math.random() * 3600 + 2000;
+		for (let i = 0; i < 20; i++) {
+			let posLimit = Math.random() * 27;
+			let randDur = Math.random() * 3600 + 2000;
 
 			let wrapper = document.createElement("a-entity");
 			wrapper.setAttribute("id", "wrapper-" + i);
@@ -32,9 +27,25 @@ AFRAME.registerComponent("boxes-setup", {
 			boxer.setAttribute("id", "boxer-" + i);
 			boxer.setAttribute("scale", this.data.boxScale);
 			boxer.setAttribute("material", { color: "#EF2D5E" });
+            boxer.setAttribute("position", {
+                x: Math.random() * Math.floor(posLimit) - 5,
+                y: Math.random() * Math.floor(posLimit) - 1,
+                z: Math.random() * Math.floor(-30)
+            });
+            boxer.setAttribute("animation", {
+                property: "rotation",
+                to: { x: 0, y: 360, z: 360 },
+                easing: 'easeInBounce',
+                dur: randDur,
+                loop: true
+            });
 
-            let aniMe = sceneEl.querySelector("#rotator");
-            aniMe.setAttribute('to', '0 360 0')
+			let aniMe = sceneEl.querySelector("#rotator-" + i);
+			aniMe.setAttribute('attribute', 'rotation')
+			aniMe.setAttribute('to', '0 360 0')
+			aniMe.setAttribute('dur', '10000')
+			aniMe.setAttribute('easing', 'linear')
+			aniMe.setAttribute('repeat', 'indefinite')
 
 			sceneEl.appendChild(wrapper);
 			wrapper.appendChild(inner);
