706ac686b300c41093b49e394318d1f7cc6c7c3d
Raymond Weitzenberg <raymondweitzenberg@avalon.localdomain>
2020-04-01 12:18:17 -0400

    one cube orbiting + broke out into main.js
    need to integrate animation on individual cubes then all all the others

---
+++ app/scripts/main.js
@@ -0,0 +1,48 @@
+AFRAME.registerComponent("boxes-setup", {
+	schema: {
+		boxScale: { type: "int", default: 2 },
+		thePos: { type: "vec3", default: { x: 1, y: 1, z: -5 } },
+		newPos: { type: "vec3", default: { x: 0, y: 0, z: -8 } }
+	},
+
+	init: function() {
+		// console.log(this.el);
+
+		const sceneEl = document.querySelector("a-scene");
+		const protoBox = sceneEl.querySelector("#oneBox");
+		console.log("sceneEl: " + sceneEl);
+
+		// const boxRotat = protoBox.components.animation__rotation;
+
+		// const posLimit = 17;
+		const scaleLimit = 3;
+
+		for (let i = 0; i < 1; i++) {
+			// let posLimit = Math.random() * 27;
+			// let randDur = Math.random() * 3600 + 2000;
+
+			let wrapper = document.createElement("a-entity");
+			wrapper.setAttribute("id", "wrapper-" + i);
+
+			let inner = document.createElement("a-entity");
+			inner.setAttribute("id", "inner-" + i);
+			inner.setAttribute("position", "0 0 -5");
+
+			let boxer = document.createElement("a-box");
+			boxer.setAttribute("id", "boxer-" + i);
+			// boxer.setAttribute('position', { x: 0, y: 0, z: -5 })
+			boxer.setAttribute("scale", { x: 2, y: 2, z: 2 });
+			boxer.setAttribute("material", { color: "#EF2D5E" });
+
+            let aniMe = sceneEl.querySelector("#rotator");
+            aniMe.setAttribute('to', '0 360 0')
+
+			sceneEl.appendChild(wrapper);
+			wrapper.appendChild(inner);
+			inner.appendChild(boxer);
+			wrapper.appendChild(aniMe);
+		}
+	},
+
+	tick: function() {}
+});
