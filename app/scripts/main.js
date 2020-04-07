da0cde7d7b786fb25a7469204300521f7d644170
Raymond Weitzenberg <raymondweitzenberg@avalon.localdomain>
2020-04-07 06:09:56 -0400

    kinetic and static asteroid cubes
    have pink cubes fixed in space and rotating in sync + blue cubes in
    motion orbiting camera
    
    need to figure out how to fork the repo as this is very different from
    where I'd left off

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,33 +6,32 @@ AFRAME.registerComponent("twilight-pink", {
 	schema: {
 		randDur: { type: "int", default: Math.floor(Math.random() * 8600 + 6500) },
 		boxScale: { type: "vec3", default: { x: 2, y: 2, z: 2 } },
-		starScaleAll: { type: "int", default: 2 }
-		starScale: { type: "vec3", default: { x: starScaleAll, y: starScaleAll, z: starScaleAll } }
+		starScale: { type: "vec3", default: { x: 2, y: 2, z: 2 } }
 	},
 
 	init: function() {
 
 		const sceneEl = document.querySelector("a-scene");
-
+		let randDur = Math.random() * 8600 + 6500;
 
 		// ————————————————————————————————————o————————————————————————————————————o Cubing -->
 		// Cubing -->
 		// ————————————————————————————————————o————————————————————————————————————o Cubing —>
 		// 
-		for (let i = 0; i < 20; i++) {
+		for (let i = 0; i < 60; i++) {
+
+			let randDur = Math.random() * 8600 + 6500;
 
 			// ————————————————————————————————————o Need boxHldr to enable orbting -->
 			// Need boxHldr to enable orbting animation -->
 			//
-			// let randDur = Math.random() * 8600 + 6500;
-
 			let boxHldr = document.createElement("a-entity");
 			boxHldr.setAttribute("id", "boxHldr-" + i);
 			boxHldr.setAttribute("animation", {
 				property: "rotation",
 				to: "0 360 0",	// Orbit around Y axis
 				easing: "linear",
-				dur: this.data.randDur,	// Random duration of orbit
+				dur: randDur,	// Random duration of orbit
 				loop: true
 			});
 
@@ -46,18 +45,19 @@ AFRAME.registerComponent("twilight-pink", {
 			let boxer = document.createElement("a-box");
 			boxer.setAttribute("id", "boxer-" + i);
 			boxer.setAttribute("scale", this.data.boxScale);
-			boxer.setAttribute("material", { color: "#EF2D5E" });
+			boxer.setAttribute("material", { color: "#37428A" });
 			boxer.setAttribute("position", { x: boxX, y: boxY, z: boxZ })
 			boxer.setAttribute("animation", {
 				property: "rotation",
 				to: "360 360 360",
 				easing: "linear",
-				dur: this.data.randDur,
+				dur: randDur,
 				loop: true
 			});
 
 			sceneEl.appendChild(boxHldr);
-			boxHldr.appendChild(boxer);
+			boxHldr.appendChild(boxer);	// Put boxer in a parent container to achieve orbital rotation
+			// I can't kick this feelin when it hits
 		}
 
 		// ————————————————————————————————————o————————————————————————————————————o Starfield -->
@@ -65,22 +65,30 @@ AFRAME.registerComponent("twilight-pink", {
 		// ————————————————————————————————————o————————————————————————————————————o Starfield —>
 		// 
 		for (let j = 0; j < 500; j++) {
+
+			// let randDur = Math.random() * 8600 + 6500;	// Using randDur defined above to keep rotations of all stars in sync
 			
 			let starX = Math.floor(Math.random() * Math.floor(-60) + 30)
 			let starY = Math.floor(Math.random() * Math.floor(-60) + 30)
-			let starZ = Math.floor(Math.random() * Math.floor(-60) + 30)
+			let starZ = Math.floor(Math.random() * Math.floor(-60) + 40)
+			let starScaleAll = 2
 			let starPos = Math.floor(Math.random() * Math.floor(-60) + 30)
 			
 			let oneStar = document.createElement("a-box");
 			oneStar.setAttribute("id", "oneStar-" + j);
 			oneStar.setAttribute("scale", this.data.starScale);
 			oneStar.setAttribute("material", { color: "#EF2D5E" });
-			// oneStar.setAttribute("position", { x: starX, y: starY, z: starZ })
-			oneStar.setAttribute("position", { x: starPos, y: starPos, z: starPos })
+			oneStar.setAttribute("position", { x: starX, y: starY, z: starZ })
+			oneStar.setAttribute("animation", {
+				property: "rotation",
+				from: "180 180 180",
+				to: "360 360 360",
+				easing: "linear",
+				dur: randDur,
+				loop: true
+			});
 
 			sceneEl.appendChild(oneStar);
 		}
-	},
-
-	tick: function() {}
+	}
 });
